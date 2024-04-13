7998e52b19b618eed39f6958b54cc028d78d8b67
Randall Naar <rnd.naar@gmail.com>
2024-04-13 08:23:38 -0400

    Cleaned up definitions in universal-tokens

--- lex/universal-tokens.ts
+++ lex/universal-tokens.ts
@@ -1,26 +1,8 @@
 import { lexKeyword, lexNumber, lexString } from "./lex-utils";
-import { eat } from "../parse/parse-utils";
 import { LexState, Token } from "../types";
 
 export class OPEN_PAREN {
   static tokenType = "OPEN_PAREN";
-  children: any[];
-  toScript?: (() => string) | undefined;
-
-  constructor() {
-    this.children = [];
-  }
-
-  static parse = (tokens: Token[]) => {
-    if (tokens[0].tokenType == OPEN_PAREN.tokenType) {
-      return tokens.shift();
-    }
-
-    throw new Error(
-      `Expected "(" at position ${tokens[0].position} but found ${tokens[0].tokenType}`
-    );
-  };
-
   static lex = (s: string, state: LexState): Token | undefined => {
     let position = state.cursor;
     if (lexKeyword(s, "(", state)) {
@@ -34,26 +16,6 @@ export class OPEN_PAREN {
 
 export class CLOSE_PAREN {
   static tokenType = "CLOSE_PAREN";
-  children: any[];
-  toScript?: (() => string) | undefined;
-
-  constructor() {
-    this.children = [];
-  }
-
-  static parse = (tokens: Token[]) => {
-    if (tokens[0].tokenType == this.tokenType) {
-      tokens.shift();
-      return;
-    }
-
-    throw new Error(
-      `Expected ")" at position ${tokens[0].position}, found ${JSON.stringify(
-        tokens[0]
-      )} instead`
-    );
-  };
-
   static lex = (s: string, state: LexState): Token | undefined => {
     let position = state.cursor;
     if (lexKeyword(s, ")", state)) {
@@ -67,22 +29,6 @@ export class CLOSE_PAREN {
 
 export class COMMA {
   static tokenType = "COMMA";
-  children: any[];
-  toScript?: (() => string) | undefined;
-
-  constructor() {
-    this.children = [];
-  }
-
-  static parse = (tokens: Token[]) => {
-    if (tokens[0].tokenType == this.tokenType) {
-      tokens.shift();
-      return;
-    }
-
-    throw new Error(`Expected "," at position ${tokens[0].position}`);
-  };
-
   static lex = (s: string, state: LexState): Token | undefined => {
     let position = state.cursor;
     if (lexKeyword(s, ",", state)) {
@@ -96,17 +42,6 @@ export class COMMA {
 
 export class COLON {
   static tokenType = "COLON";
-  children: any[];
-  toScript?: (() => string) | undefined;
-
-  constructor() {
-    this.children = [];
-  }
-
-  static parse = (tokens: Token[]) => {
-    eat(tokens, this.tokenType);
-  };
-
   static lex = (s: string, state: LexState): Token | undefined => {
     let position = state.cursor;
     if (lexKeyword(s, ":", state)) {
@@ -120,19 +55,6 @@ export class COLON {
 
 export class NUMBER {
   static tokenType = "NUM";
-  children: any[];
-  toScript?: (() => string) | undefined;
-
-  constructor() {
-    this.children = [];
-  }
-
-  static parse = (tokens: any) => {
-    if (tokens[0].tokenType == this.tokenType) {
-      return tokens.shift();
-    }
-  };
-
   static lex = (s: string, state: LexState): Token | undefined => {
     let position = state.cursor;
     let num = lexNumber(s, state);
@@ -148,22 +70,6 @@ export class NUMBER {
 
 export class STRING {
   static tokenType = "STRING";
-  children: any[];
-
-  constructor() {
-    this.children = [];
-  }
-
-  static parse = (tokens: Token[]) => {
-    if (tokens[0].tokenType == STRING.tokenType) {
-      return tokens.shift();
-    }
-
-    throw new Error(
-      `Expected string at position ${tokens[0].position} but found ${tokens[0].tokenType}`
-    );
-  };
-
   static lex = (s: string, state: LexState): Token | undefined => {
     let position = state.cursor;
     let str = lexString(s, state);
